a862ccab685588aac7bb055e745fe5b31fc31bc4
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-02-20 16:55:57 -0500

    fixing

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
+
 var dateBox = document.getElementById('customDate');
 const date = new Date().toDateString();
 dateBox.innerHTML = date;
 
 
+let savebutton = document.getElementById('save');
+savebutton.addEventListener('click', function() {
+    
+  var taskList = document.getElementById('test').textContent;
+  localStorage.setItem('taskLists', taskList);
+  console.log(taskList);
+})
+
+let loadbutton = document.getElementById('load');
+loadbutton.addEventListener('click', function() {
+  var taskList = document.getElementById('test');
+  var list = document.getElementById('test');
+    var names = localStorage.getItem('taskLists');
+    const nameArray = names.split();
+    nameArray.join('\n ');
+    if (names !== null) {
+      list.innerHTML = '' + nameArray;
+    }
+    else {
+      list.innerHTML = taskList;
+    }
+    console.log(names);
+    //document.getElementById('listoftask').innerHTML = taskList;
+})
+
+
 function addTask() { 
 
-    
-    
-    
-    //var list = document.createElement('div');
     var br = document.createElement('br');
 
     var task = document.getElementById('tasks');
     //list.appendChild(task.value + '\n');
     var test = document.getElementById('test');
+    test.appendChild(br.cloneNode());
     test.append('\n • \n' + task.value);
     test.appendChild(br.cloneNode());
 
@@ -21,36 +45,13 @@ function addTask() {
     let form = document.getElementById('form');
     form.style.display = 'none';
     task.value = '';
-    //var table = document.getElementById('table');
-    //table.innerHTML =  task.value;
-    //document.getElementsByTagName('form')[0].appendChild(task.value);
+
 
 };
 
 
 function addAnother() {
 
-    /*var br = document.createElement("br");
-
-    var form = document.createElement('form');
-    form.setAttribute('name', 'task');
-
-    var input = document.createElement('input')
-    input.setAttribute("type", "text");
-    input.setAttribute('class', 'tasks')
-    input.setAttribute("placeholder", "Your Task Here");
-    input.setAttribute("value", "");
-
-    var submit = document.createElement('input');
-    submit.setAttribute('type', 'submit');
-    submit.setAttribute('value', 'Add');
-    submit.onclick = addTask();
-
-    form.appendChild(input);
-    form.appendChild(submit);
-    form.appendChild(br.cloneNode());
-    document.getElementById('test').innerHTML = form;
-    */
    let form = document.getElementById('form');
    form.style.display = 'block';
    
