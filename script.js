0fabbc9215f6278c4d17e3f371a8bc561829e416
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-02-20 11:47:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,56 +2,56 @@ var dateBox = document.getElementById('customDate');
 const date = new Date().toDateString();
 dateBox.innerHTML = date;
 
+
 function addTask() { 
 
     
     
     
     //var list = document.createElement('div');
+    var br = document.createElement('br');
+
     var task = document.getElementById('tasks');
     //list.appendChild(task.value + '\n');
     var test = document.getElementById('test');
-    test.innerHTML = task.value;
+    test.append('\n • \n' + task.value);
+    test.appendChild(br.cloneNode());
+
+
+    let form = document.getElementById('form');
+    form.style.display = 'none';
+    task.value = '';
     //var table = document.getElementById('table');
     //table.innerHTML =  task.value;
     //document.getElementsByTagName('form')[0].appendChild(task.value);
 
 };
 
-/*function addTask() {
-    //var table = document.getElementById('table');
-    
-    document.getElementById('bodyContent').appendChild(list);
-    var list = document.createElement('input');
-    
-    
-    list.append("valuetask.value + '\n'");
-    //table.appendChild(list);
-    
-    //list.setAttribute
-    var task = document.getElementById('tasks');
-    
-}
-*/
+
 function addAnother() {
 
-    var br = document.createElement("br");
+    /*var br = document.createElement("br");
+
     var form = document.createElement('form');
-      form.setAttribute('name', 'task');
+    form.setAttribute('name', 'task');
+
     var input = document.createElement('input')
     input.setAttribute("type", "text");
     input.setAttribute('class', 'tasks')
-    //form.setAttribute("name", "");
     input.setAttribute("placeholder", "Your Task Here");
     input.setAttribute("value", "");
+
     var submit = document.createElement('input');
     submit.setAttribute('type', 'submit');
     submit.setAttribute('value', 'Add');
     submit.onclick = addTask();
-    //submit.setAttribute('onclick', 'addTask()');
-    form.appendChild(input);
 
+    form.appendChild(input);
     form.appendChild(submit);
     form.appendChild(br.cloneNode());
-    document.getElementsByTagName("div")[2].appendChild(form);
+    document.getElementById('test').innerHTML = form;
+    */
+   let form = document.getElementById('form');
+   form.style.display = 'block';
+   
 }
