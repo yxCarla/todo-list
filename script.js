06fd4a624b562dd40aa01f7d2ecdd9d39bb6685b
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-02-20 23:29:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
-
-var dateBox = document.getElementById('customDate');
-const date = new Date().toDateString();
+const dateBox = document.getElementById('customDate');
+var date = new Date().toDateString();
+var dateTime = new Date().toLocaleString();
 dateBox.innerHTML = date;
 
-
 let savebutton = document.getElementById('save');
-savebutton.addEventListener('click', function() {
-    
+savebutton.addEventListener('click', function () {
+
   var taskList = document.getElementById('test').textContent;
   localStorage.setItem('taskLists', taskList);
+  // console log for testing
   console.log(taskList);
+  window.alert('To-Do List successfully SAVED at ' + dateTime + '!');
 })
 
 let loadbutton = document.getElementById('load');
 loadbutton.addEventListener('click', function() {
-  var taskList = document.getElementById('test');
+  var taskList = document.getElementById('test').textContent;
   var list = document.getElementById('test');
+  var prealert = document.getElementById('alert');
+  //var br = document.createElement('br');
+  //test.appendChild(br.cloneNode());
     var names = localStorage.getItem('taskLists');
-    const nameArray = names.split();
-    nameArray.join('\n ');
-    if (names !== null) {
-      list.innerHTML = '' + nameArray;
+    //names.concat(br.cloneNode());
+    const nameArray = names.split('\n •');
+    let nameArrayJoined = '<br>' + nameArray.join('<br>\n •');
+    if (names.value = 'undefined') {
+      prealert.innerHTML = 'Whoops! There are no previous lists!';
+      console.log('No Returned Data');
     }
     else {
-      list.innerHTML = taskList;
+      list.innerHTML = nameArrayJoined;
+      var p = document.getElementsByTagName('p')[0];
+      p.style.display = 'none';
     }
+    
     console.log(names);
     //document.getElementById('listoftask').innerHTML = taskList;
+})
+
+let clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', function() {
+  var taskList = document.getElementById('test').textContent;
+  var prealert = document.getElementById('alert');
+  prealert.innerHTML = '';
+  var list = document.getElementById('test');
+  localStorage.setItem('taskLists', '')
+  list.innerHTML = '';
+  var p = document.getElementsByTagName('p')[0];
+  p.style.display = 'block';
+  if (p.style.display = 'block') {
+    window.alert('To-Do List successfully CLEARED!');
+  }
 })
 
 
 function addTask() { 
 
     var br = document.createElement('br');
-
+    var p = document.getElementsByTagName('p')[0];
+    p.style.display = 'none';
     var task = document.getElementById('tasks');
     //list.appendChild(task.value + '\n');
     var test = document.getElementById('test');
-    test.appendChild(br.cloneNode());
-    test.append('\n • \n' + task.value);
+    //test.appendChild(br.cloneNode());
+    test.append('\n • ' + task.value);
     test.appendChild(br.cloneNode());
 
 
