6d88b16f9e131b1892aa2ae1b59d2b7817c34934
yxCarla <70400085+yxCarla@users.noreply.github.com>
2022-02-21 23:30:28 -0500

    attempt to fix bug

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ savebutton.addEventListener('click', function () {
   console.log(taskList);
   window.alert('To-Do List successfully SAVED at ' + dateTime + '!');
   //}
-  
+
 })
-  
+
 
 // load button function 
 
 let loadbutton = document.getElementById('load');
-loadbutton.addEventListener('click', function() {
+loadbutton.addEventListener('click', function () {
   var taskList = document.getElementById('test').textContent;
   var list = document.getElementById('test');
   var prealert = document.getElementById('alert');
@@ -41,63 +41,64 @@ loadbutton.addEventListener('click', function() {
   //var br = document.createElement('br');
   //test.appendChild(br.cloneNode());
 
-    var names = localStorage.getItem('taskLists');
-    var checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    
-    //names.concat(br.cloneNode());
-    const nameArray = names.split('(delete)');
-    //nameArray.forEach(addCheck);
+  var names = localStorage.getItem('taskLists');
+  var checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+
+  //names.concat(br.cloneNode());
+  const nameArray = names.split('a');
+  nameArray.forEach(addCheck);
   console.log(nameArray);
 
-    
-    let nameArrayJoined = '<br>' + nameArray.join(' <br>\n');
-    console.log(nameArrayJoined);
-    console.log(names);
-    // error if no previous list
-    if ((names === '') || (taskList = '')) {
-      prealert.innerHTML = 'Whoops! There are no previous lists! Create a new list by clicking "Add Task"';
-      //var prealert = document.getElementById('alert');
-      //prealert.style.display = 'none';
-      console.log('No Returned Data');
-    }
-    else {
-      //var prealert = document.getElementById('alert');
-      prealert.style.display = 'none';
-      list.innerHTML = nameArrayJoined;
-      
-    }
-    
-    // clearing input form
-    let form = document.getElementById('form');
-    form.style.display = 'none';
 
-    
-    //document.getElementById('listoftask').innerHTML = taskList;
+  let nameArrayJoined = '<br>' + nameArray.join('<br>\n •');
+  console.log(nameArrayJoined);
+  console.log(names);
+  // error if no previous list
+  if ((names === '') || (taskList = '')) {
+    prealert.innerHTML = 'Whoops! There are no previous lists! Create a new list by clicking "Add Task"';
+    //var prealert = document.getElementById('alert');
+    //prealert.style.display = 'none';
+    console.log('No Returned Data');
+  }
+  else {
+    //var prealert = document.getElementById('alert');
+    prealert.style.display = 'none';
+    list.innerHTML = nameArrayJoined;
+
+  }
+
+  // clearing input form
+  let form = document.getElementById('form');
+  form.style.display = 'none';
+
+
+  //document.getElementById('listoftask').innerHTML = taskList;
 })
 
-/*function addCheck(value) {
+function addCheck(value) {
   var names = localStorage.getItem('taskLists');
-const nameArray = names.split('(delete)');
-var checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    
-    //let nameArrayJoined = '<br>' + nameArray.join(' <br>\n') + '(delete)';
-  let nameArrayJoined = checkbox.append(value);
+  const nameArray = names.split('a');
+  console.log(nameArray); // THIS ONE
+  var checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+
+  let nameArrayJoined = '<br>' + nameArray.join(' <br>\n') + '(delete)';
+  nameArrayJoined += checkbox, nameArrayJoined;
   console.log(nameArrayJoined);
 }
-*/
+
 
 // clear button function 
 
 let clearButton = document.getElementById('clear');
-clearButton.addEventListener('click', function() {
+clearButton.addEventListener('click', function () {
   var taskList = document.getElementById('test').textContent;
   taskList = '';
   var prealert = document.getElementById('alert');
   prealert.innerHTML = 'No Tasks Yet!';
   var list = document.getElementById('test');
-  
+
   localStorage.setItem('taskLists', taskList);
   var names = localStorage.getItem('taskLists');
   list.innerHTML = names;
@@ -115,29 +116,29 @@ clearButton.addEventListener('click', function() {
 
 // to add a task
 
-function addTask() { 
+function addTask() {
 
-    var br = document.createElement('br');
-    var prealert = document.getElementById('alert');
-    prealert.innerHTML = '';
+  var br = document.createElement('br');
+  var prealert = document.getElementById('alert');
+  prealert.innerHTML = '';
 
-    var checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.onclick = checkTick();
-    //var p = document.getElementsByTagName('p')[0];
-    //p.style.display = 'none';
-    var task = document.getElementById('tasks');
-    //list.appendChild(task.value + '\n');
-    var test = document.getElementById('test');
-    //test.appendChild(br.cloneNode());
-    test.append(checkbox, task.value, ' (delete)', br.cloneNode());
-    //test.appendChild(br.cloneNode());
-    console.log(test);
-    
+  var checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.onclick = checkTick();
+  //var p = document.getElementsByTagName('p')[0];
+  //p.style.display = 'none';
+  var task = document.getElementById('tasks');
+  //list.appendChild(task.value + '\n');
+  var test = document.getElementById('test');
+  //test.appendChild(br.cloneNode());
+  test.append(checkbox, task.value, ' (delete)', br.cloneNode());
+  //test.appendChild(br.cloneNode());
+  console.log(test);
 
-    let form = document.getElementById('form');
-    form.style.display = 'none';
-    task.value = '';
+
+  let form = document.getElementById('form');
+  form.style.display = 'none';
+  task.value = '';
 
 
 }
